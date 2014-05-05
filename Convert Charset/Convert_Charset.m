2a80b81972dd77a10ef99a2e77a1d8831c7f3e21
Jakub Hampl <honitom@seznam.cz>
2014-05-05 18:22:09 +0100

    Setup

--- Convert Charset/Convert_Charset.m
+++ Convert Charset/Convert_Charset.m
@@ -7,13 +7,30 @@
 //
 
 #import "Convert_Charset.h"
+#import <OSAKit/OSAKit.h>
 
 @implementation Convert_Charset
 
 - (id)runWithInput:(id)input fromAction:(AMAction *)anAction error:(NSDictionary **)errorInfo
 {
-	// Add your code here, returning the data to be passed to the next action.
-	
+    NSError *error;
+	for (NSString *path in input) {
+        NSNumber *from = [self parameters][@"from"];
+        NSNumber *to = [self parameters][@"to"];
+        NSString *data = [NSString stringWithContentsOfFile:path encoding: [from longValue] error:&error];
+        if (data) {
+            BOOL ok = [data writeToFile:path atomically:YES
+                               encoding:[to longValue] error:&error];
+            if (!ok) {
+                *errorInfo = @{ OSAScriptErrorNumber: @(errOSASystemError), OSAScriptErrorMessage :[NSString stringWithFormat:@"ERROR: Could not convert file: %@\n", path]};
+                return nil;
+            }
+        } else {
+            *errorInfo = @{ OSAScriptErrorNumber: @(errOSASystemError), OSAScriptErrorMessage :[NSString stringWithFormat:@"ERROR: Could not convert file: %@\n", path] };
+            return nil;
+        }
+        
+    }
 	return input;
 }
 
